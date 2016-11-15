04a510a2a09953fe6a51af8f5ff4ea108421cef5
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2016-11-15 15:36:39 +0100

    Update for 1.11 release

--- src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
+++ src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
@@ -29,7 +29,7 @@ import org.lanternpowered.server.network.message.MessageRegistry;
 
 public interface Protocol {
 
-    int CURRENT_VERSION = 313;
+    int CURRENT_VERSION = 315;
 
     /**
      * Gets the inbound message registry.

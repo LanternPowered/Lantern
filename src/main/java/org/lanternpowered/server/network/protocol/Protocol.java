8b827d3fc879130b998e63d5579d8d6206907012
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2017-05-13 12:38:17 +0200

    Update for Minecraft 1.12 pre-release 2

--- src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
+++ src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
@@ -29,7 +29,7 @@ import org.lanternpowered.server.network.message.MessageRegistry;
 
 public interface Protocol {
 
-    int CURRENT_VERSION = 328;
+    int CURRENT_VERSION = 329;
 
     /**
      * Gets the inbound message registry.
--- src/main/java/org/lanternpowered/server/plugin/InternalPluginsInfo.java
+++ src/main/java/org/lanternpowered/server/plugin/InternalPluginsInfo.java
@@ -71,7 +71,7 @@ public final class InternalPluginsInfo {
 
         public static final String NAME = "Minecraft";
         public static final String IDENTIFIER = "minecraft";
-        public static final String VERSION = "1.12-pre1";
+        public static final String VERSION = "1.12-pre2";
 
         private Minecraft() {
         }

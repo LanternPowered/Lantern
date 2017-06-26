ea72b158d165308c404d3a2a83255fcdf8d51ce5
Parker Lougheed <parkermeronat@gmail.com>
2017-06-26 18:33:09 -0500

    Remove uneeded suppression

--- src/main/java/org/lanternpowered/server/config/serializer/CatalogTypeSerializer.java
+++ src/main/java/org/lanternpowered/server/config/serializer/CatalogTypeSerializer.java
@@ -33,8 +33,7 @@ import org.spongepowered.api.CatalogType;
 import org.spongepowered.api.Sponge;
 
 public final class CatalogTypeSerializer implements TypeSerializer<CatalogType> {
-
-    @SuppressWarnings("unchecked")
+    
     @Override
     public CatalogType deserialize(TypeToken<?> type, ConfigurationNode value) throws ObjectMappingException {
         return Sponge.getRegistry().getType(type.getRawType().asSubclass(CatalogType.class), value.getString())

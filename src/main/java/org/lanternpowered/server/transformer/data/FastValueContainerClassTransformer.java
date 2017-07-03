9ed990925703157ce4957455d7d2cced0f789447
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2017-07-03 23:02:04 +0200

    Added some info

--- src/main/java/org/lanternpowered/server/transformer/data/FastValueContainerClassTransformer.java
+++ src/main/java/org/lanternpowered/server/transformer/data/FastValueContainerClassTransformer.java
@@ -26,10 +26,84 @@
 package org.lanternpowered.server.transformer.data;
 
 import org.lanternpowered.launch.transformer.ClassTransformer;
+import org.lanternpowered.server.data.CompositeValueStoreHelper;
+import org.lanternpowered.server.data.ICompositeValueStore;
+import org.lanternpowered.server.entity.LanternEntity;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.api.data.DataTransactionResult;
+import org.spongepowered.api.data.value.mutable.CompositeValueStore;
+import org.spongepowered.api.entity.Entity;
 
+/**
+ * This {@link ClassTransformer} attempts to replace method calls inside {@link CompositeValueStore}s
+ * that return {@link DataTransactionResult}. A lot of times are these {@link DataTransactionResult}s
+ * discarded and this make it pointless to generate these. Replacing these methods with fast equivalents
+ * in these cases that avoid {@link DataTransactionResult} object creations. Fast methods will always
+ * return a {@code boolean} value that represents if the action was successful.
+ * <p>All the fast methods are present in the {@link ICompositeValueStore}, every {@link CompositeValueStore}
+ * should use this expansion interface to support fast methods. If this isn't the case it will fall back
+ * to the original methods.
+ * <p>Some examples:
+ * <p>This is a simple example where you offer health to a entity. The offer method returns
+ * a {@link DataTransactionResult} but is never used.
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * entity.offer(Keys.HEALTH, 2.0);
+ * }
+ * </pre>
+ * The code above will be replaced by:
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * CompositeValueStoreHelper.offer(entity, Keys.HEALTH, 2.0);
+ * }
+ * </pre>
+ * A helper class ({@link CompositeValueStoreHelper}) is here used to check if the {@link Entity} the
+ * {@link ICompositeValueStore} value extends and then calls the fast method, otherwise it will fall
+ * back to the original method. The helper class makes it easy to transform the code, without adding
+ * extra lines in it, only the method call has to be modified.
+ * <p>
+ * Another example, we know in this case that the {@link Entity} is a {@link ICompositeValueStore},
+ * because it is first casted to {@link LanternEntity} which implements that interface.
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * final LanternEntity entity1 = (LanternEntity) entity;
+ * entity1.offer(Keys.HEALTH, 2.0);
+ * }
+ * </pre>
+ * This will be replaced by:
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * final LanternEntity entity1 = (LanternEntity) entity;
+ * entity1.offerFast(Keys.HEALTH, 2.0);
+ * }
+ * </pre>
+ * <p>
+ * Then is there also a case where the {@link DataTransactionResult} is actually used, but only to
+ * check if it was successful ({@link DataTransactionResult#isSuccessful()}). This can also be represented
+ * by the {@code boolean} value that is returned by the fast methods. In this case will the following
+ * transformation occur.
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * final LanternEntity entity1 = (LanternEntity) entity;
+ * boolean success = entity1.offer(Keys.HEALTH, 2.0).isSuccessful();
+ * }
+ * </pre>
+ * This will be replaced by:
+ * <pre>
+ * {@code
+ * final Entity entity = ...;
+ * final LanternEntity entity1 = (LanternEntity) entity;
+ * boolean success = entity1.offerFast(Keys.HEALTH, 2.0);
+ * }
+ * </pre>
+ */
 public final class FastValueContainerClassTransformer implements ClassTransformer {
 
     @Override
